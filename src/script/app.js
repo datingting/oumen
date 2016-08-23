4f5b1145a378605cc3d972644335f54e20a4e73a
maxingkai <15634359372@163.com>
2016-08-23 21:28:04 +0800

    1.0版本

--- src/script/app.js
+++ src/script/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2016/8/19.
  */
-require("./libs/iscroll.js");
+
 require("./libs/spa.min.js");
 require("./libs/swiper-3.3.1.min.js");
 
--- src/script/views/my.js
+++ src/script/views/my.js
@@ -6,8 +6,3 @@ var indexMy = require("../tpls/my.string");
 SPA.defineView("my", {
     html: indexMy
 });
-var myscroll = new iScroll('wrapper',{
-	checkDOMChanges:true,
-	vScrollbar:false,
-	hideScrollbar:true
-})
